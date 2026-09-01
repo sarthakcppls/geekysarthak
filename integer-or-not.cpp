a80299fd091633b4629e58d7b730011bde49277d
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-01 19:58:24 +0530

    Create integer-or-not.cpp

---
+++ integer-or-not.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+      cout<<"enter the number: ";
+      float number;
+      cin>>number;
+     if (float(number) == int(number)) {
+           cout<<"is integer";
+          
+     }
+      else cout<<"not integer";
+}
