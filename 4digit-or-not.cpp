86b5b937bee378b737e0479da049b6cba2dc98c3
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-01 19:59:51 +0530

    Create 4digit-or-not.cpp

---
+++ 4digit-or-not.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+       // take an integer input and tell if it is 4 digit or not
+       int a;
+       cout<<"enter a number: ";
+       cin>> a;
+       if (a>=1000 and a<=9999) cout<<"4 digit number";
+       else cout<<"not a 4 digit number";
+      
+}
