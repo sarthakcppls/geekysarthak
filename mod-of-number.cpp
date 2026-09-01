4dffc8368c45b28eefe103eef88aa0c3eefdf766
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-01 19:54:19 +0530

    Create mod-of-number.cpp

---
+++ mod-of-number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+      cout<<"enter the number: ";
+      int number;
+      cin>>number;
+      if (number > 0 ) cout<<number;
+      else cout<<-number;
+      
+}
