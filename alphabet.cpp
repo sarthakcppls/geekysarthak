3f98fb5a0391c76f02dbf1015cc0e3c60b4bdef2
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-04 21:39:32 +0530

    Create alphabet.cpp

---
+++ alphabet.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+       // capital alphabet or not
+       char a;
+       cout<<"enter the letter: ";
+       cin>>a;
+       if((char)a >=65 and (char)a <=90) cout<<"capital alphabet";
+       else cout<<"nigga its not";
+}
