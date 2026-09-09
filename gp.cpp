a4c8ac156b0591b20ec00be359ee2d16682d9bb6
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-09 20:05:52 +0530

    Create gp.cpp

---
+++ gp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+      cout<<"enter the number: ";
+      int a;
+      cin>>a;
+      for(int i = 1; i<=a; i*=2) {
+            cout<<i<<"  ";
+      }
+}
