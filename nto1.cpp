b8d7e712387054ea21620bc4f27dcf952fed7a97
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-09 20:09:00 +0530

    Create nto1.cpp

---
+++ nto1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+      cout<<"enter the number ";
+      int n;
+      cin>>n;
+      for (int i=2; i<=n; i+=3){
+            cout<<i<<"  ";
+      }
+}
