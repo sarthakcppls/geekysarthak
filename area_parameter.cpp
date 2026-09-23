d7b2dc26bd673710f6f632a3cd309400d5ba7be2
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-23 19:51:29 +0530

    Create area_parameter.cpp

---
+++ area_parameter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+      // Calculate Area & Perimeter of Rectangle:
+      int length,breath;
+      cout<<"enter length: ";
+      cin>>length;
+      cout<<"enter breath: ";
+      cin>>breath;
+      cout<<"area of Rectangle: "<<length*breath<<endl;
+      cout<<"parameter of Rectangle: "<<2*(length+breath);
+      
+}
