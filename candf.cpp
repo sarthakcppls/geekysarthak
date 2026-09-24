a2f049f13d870e02c95557d002cc48ea79583ba3
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-24 19:40:28 +0530

    Create candf.cpp

---
+++ candf.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+      float c,f;
+      cout<<"enter the temprature in c: ";
+      cin>>c;
+      cout<<"the temprature in f: ";
+      f=32+(c*(9/5));
+      cout<<f;
+      
+}
