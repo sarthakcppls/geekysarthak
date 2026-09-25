d652de4f38d19c87ba2c55b043887e3c68010644
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-25 14:21:23 +0530

    Create areaofcircle.cpp

---
+++ areaofcircle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+      float pi =3.14;
+      float r,a;
+      cout<<"enter radius: ";
+      cin>>r;
+      a=pi*r*r;
+      cout<<"area of circle: "<<a;
+      
+}
