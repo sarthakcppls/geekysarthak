a16fb316e925d74e6ed4b77623e5580680339841
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-09-25 12:27:18 +0530

    Create idk.cpp

---
+++ idk.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+      int si,p,t,r;
+      cout<<"enter the p: ";
+      cin>>p;
+      cout<<"enter the t: ";
+      cin>>t;
+      cout<<"enter the r: ";
+      cin>>r;
+      si = (p*r*t)/100;
+      cout<<"the value of si: "<<si;
+}
